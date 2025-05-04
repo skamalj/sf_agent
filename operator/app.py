a149c835df05d861c15ed85769ad199c86bc4872
kamal <kamal@kamal>
2025-05-04 11:16:13 +0000

    fix

--- operator/app.py
+++ operator/app.py
@@ -120,7 +120,7 @@ def handle_message(channel_type, recipient, message):
 
     config = {"configurable": {"thread_id": profile_id}}
     response = app.invoke(input_message, config)
-    print("Unparsed Response History - last 7:", response[-7:])
+    print("Unparsed Response History - last 7:", response["messages"][-7:])
     # Step 4: Parse response from Comms-Agent and construct final return response
     agent_response = response["messages"][-1].content
     print("Unparsed Response:", agent_response)
